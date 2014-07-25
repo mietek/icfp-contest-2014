207b0ce64bd24e6d9e027fa2c8758f73ec560f54
Arek Flinik <aflinik@gmail.com>
2014-07-26 01:52:43 +0200

    machine code preprocessor courtesy of @jdudek

---
+++ preprocessor.rb
@@ -0,0 +1,39 @@
+class Preprocessor
+  def input
+    $stdin
+  end
+
+  def output
+    lines.map do |line|
+      if line =~ /^LDF @(\w+)$/
+        "LDF #{line_number_of($1)}\n"
+      else
+        line
+      end
+    end.join
+  end
+
+  def lines
+    @lines ||= input.lines.to_a
+  end
+
+  def non_empty_lines
+    lines.select { |line| line.strip.length > 0 }
+  end
+
+  def line_number_of(label)
+    labels[label] or raise "Unknown label: #{label}"
+  end
+
+  def labels
+    @labels ||= Hash[labels_with_line_numbers]
+  end
+
+  def labels_with_line_numbers
+    non_empty_lines.each_with_index
+      .map { |line, number| [$1, number] if line =~ /\; @(\w+)/ }
+      .compact
+  end
+end
+
+puts Preprocessor.new.output
